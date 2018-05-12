8eb86aef58857d4e3c0d2017ec6470b8c784f0b1
Jhonata Quinonez Avila <jquinon1@eafit.edu.co>
2018-05-12 09:19:53 -0500

    First commit

---
+++ verify.py
@@ -0,0 +1,27 @@
+#This scripy verify is all lines start with the correct structure
+#\#,id,title,publication,author,date,year,month,url,content
+
+import re
+
+file_name = 'articles3'
+with open("{}.csv".format(file_name)) as f:
+    counter = 1
+    bad = 0
+    for line in f:
+        if line.startswith(('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')):
+            counter = counter+1
+        else:
+            print(counter)
+            counter = counter+1
+            bad = bad+1
+print(bad)
+if bad > 1:
+    print("Creating a file with the standard")
+    with open("{}_cleaned.csv".format(file_name),'w') as w:
+        with open("{}.csv".format(file_name)) as f:
+            for line in f:
+                if not re.match(r'^\s*$', line):
+                    if line.startswith(('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')):
+                        w.write("\n{}".format(line.rstrip()))
+                    else:
+                        w.write(line.rstrip())

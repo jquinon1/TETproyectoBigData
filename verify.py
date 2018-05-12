190fe4c23c6cc8ecd06b86b45588491805c4a855
Jhonata Quinonez Avila <jquinon1@eafit.edu.co>
2018-05-12 11:25:39 -0500

    added datasets

--- verify.py
+++ verify.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #This scripy verify is all lines start with the correct structure
 #\#,id,title,publication,author,date,year,month,url,content
 
@@ -11,17 +13,16 @@ with open("{}.csv".format(file_name)) as f:
         if line.startswith(('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')):
             counter = counter+1
         else:
-            print(counter)
+            print("Bad line in {}".format(counter))
             counter = counter+1
             bad = bad+1
-print(bad)
-if bad > 1:
-    print("Creating a file with the standard")
-    with open("{}_cleaned.csv".format(file_name),'w') as w:
-        with open("{}.csv".format(file_name)) as f:
-            for line in f:
-                if not re.match(r'^\s*$', line):
-                    if line.startswith(('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')):
-                        w.write("\n{}".format(line.rstrip()))
-                    else:
-                        w.write(line.rstrip())
+print("Number of bad lines {}".format(bad))
+print("Creating a file with the standard")
+with open("{}_cleaned.csv".format(file_name),'w') as w:
+    with open("{}.csv".format(file_name)) as f:
+        for line in f:
+            if not re.match(r'^\s*$', line):
+                if line.startswith(('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')):
+                    w.write("\n{}".format(line.rstrip().replace("\"","").replace("”","").replace("\'","").replace("’","")))
+                else:
+                    w.write(line.rstrip().replace("\"","").replace("”","").replace("\'","").replace("’",""))

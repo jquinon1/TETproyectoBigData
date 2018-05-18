e1ec2216d01b4dd32a04990b9d5b52645830fa97
Jhonata Quinonez Avila <jquinon1@eafit.edu.co>
2018-05-18 17:25:40 -0500

    strengthening regex cleaning

--- verify.py
+++ verify.py
@@ -14,12 +14,12 @@ for i in range(1,4):
             if line.startswith(('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')):
                 counter = counter+1
             else:
-                print("Bad line in {}".format(counter))
+                #print("Bad line in {}".format(counter))
                 counter = counter+1
                 bad = bad+1
 
     print("Number of bad lines {}".format(bad))
-    print("Creating a file with the standard")
+    print("Creating a file with the standard {}{}".format(file_name,i))
     with open("{}{}_cleaned.csv".format(file_name,i),'w') as w:
         with open("{}{}.csv".format(file_name,i)) as f:
             for line in f:
@@ -28,3 +28,11 @@ for i in range(1,4):
                         w.write("\n{}".format(line.rstrip().replace('\r','')))
                     else:
                         w.write(line.rstrip().replace('\r',''))
+                    #to_wr = re.compile("\,\d{4}\-\d{2}\-\d{2}\,\d{4}\.\d\,\d+\.\d\,\"?(https?://www\.\w+\.\w+(\/[\w\d\-\.]+)+\/?)?\"?\,").split(temp)
+                    #to_wr = re.split(r'\,\d{4}\-\d{2}\-\d{2}\,\d{4}\.\d\,\d+\.\d\,\"?(https?://www\.\w+\.\w+(\/[\w\d\-\.]+)+\/?)?\"?\,',temp)
+                    #print(to_wr) if len(to_wr) == 1 else "hola"
+    with open("{}{}_cleaned.csv".format(file_name,i)) as f:
+        with open("{}_filtered.csv".format(file_name),'w') as w:
+            for line in f:
+                to_wr = re.compile("\,(\d{4}[\-\/]\d{1,2}[\-\/]\d{1,2}|nan)?\,(\d{4}\.\d|nan)?\,(\d+\.\d|nan)?\,\s*\"?\s*(https?://(\w+\.)?\w+\.\w+(\/[\w\d\-\.\=\?\&\+\(\)\%\s\#]+)+\/?)*\s*\"?\s*\,").split(line)
+                print(to_wr) if len(to_wr) == 1 else ""

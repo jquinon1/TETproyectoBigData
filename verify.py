0f34edc6503429e0211dd6a92992762b3129c4af
Jhonata Quinonez Avila <jquinon1@eafit.edu.co>
2018-05-18 10:05:41 -0500

    Updating verify script

--- verify.py
+++ verify.py
@@ -5,24 +5,26 @@
 
 import re
 
-file_name = 'articles3'
-with open("{}.csv".format(file_name)) as f:
-    counter = 1
-    bad = 0
-    for line in f:
-        if line.startswith(('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')):
-            counter = counter+1
-        else:
-            print("Bad line in {}".format(counter))
-            counter = counter+1
-            bad = bad+1
-print("Number of bad lines {}".format(bad))
-print("Creating a file with the standard")
-with open("{}_cleaned.csv".format(file_name),'w') as w:
-    with open("{}.csv".format(file_name)) as f:
+file_name = 'articles'
+for i in range(1,4):
+    with open("{}{}.csv".format(file_name,i)) as f:
+        counter = 1
+        bad = 0
         for line in f:
-            if not re.match(r'^\s*$', line):
-                if line.startswith(('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')):
-                    w.write("\n{}".format(line.rstrip().replace('\r','')))
-                else:
-                    w.write(line.rstrip().replace('\r',''))
+            if line.startswith(('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')):
+                counter = counter+1
+            else:
+                print("Bad line in {}".format(counter))
+                counter = counter+1
+                bad = bad+1
+
+    print("Number of bad lines {}".format(bad))
+    print("Creating a file with the standard")
+    with open("{}{}_cleaned.csv".format(file_name,i),'w') as w:
+        with open("{}{}.csv".format(file_name,i)) as f:
+            for line in f:
+                if not re.match(r'^\s*$', line):
+                    if line.startswith(('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')):
+                        w.write("\n{}".format(line.rstrip().replace('\r','')))
+                    else:
+                        w.write(line.rstrip().replace('\r',''))
